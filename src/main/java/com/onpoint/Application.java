2c8f29191f274fdccab38e84a32e5a0e2df83ecf
scott.johnson <scott.johnson@onpointtech.com>
2015-03-30 11:33:55 -0500

    test commit

--- src/main/java/com/onpoint/Application.java
+++ src/main/java/com/onpoint/Application.java
@@ -59,6 +59,8 @@ public class Application {
         Environment env = app.run(args).getEnvironment();
         log.info("Access URLs:\n----------------------------------------------------------\n\t" +
             "Local: \t\thttp://127.0.0.1:{}\n\t" +
+        		
+            "some stuff"
             "External: \thttp://{}:{}\n----------------------------------------------------------",
             env.getProperty("server.port"),
             InetAddress.getLocalHost().getHostAddress(),
